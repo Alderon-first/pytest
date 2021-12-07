2a46c5271f4baeac5b7d31662684418414d3680a
Alderon <reidzilag@mail.ru>
2021-12-07 14:18:57 +0300

    добавлены комментарии, убраны лишние шаги

--- test_add_group.py
+++ test_add_group.py
@@ -9,48 +9,46 @@ class TestAddGroup(unittest.TestCase):
     
     def test_add_group(self):
         wd = self.wd
+        # open home page
         wd.get("http://localhost/addressbook/")
+        # Login
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
         wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_xpath("//input[@value='Login']").click()
+        # open groups page
         wd.find_element_by_link_text("groups").click()
+        # init group creation
         wd.find_element_by_name("new").click()
+        # fill groups +
         wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").click()
-        # ERROR: Caught exception [ERROR: Unsupported command [doubleClick | name=group_name | ]]
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys("group")
         wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("secret")
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("secret")
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("secret1")
+        wd.find_element_by_name("group_header").send_keys("header")
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("secret1")
+        wd.find_element_by_name("group_footer").send_keys("footer")
+        # submit group creation
         wd.find_element_by_name("submit").click()
+        # return groups page
         wd.find_element_by_link_text("group page").click()
+        # Logout
         wd.find_element_by_link_text("Logout").click()
     
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
+    #def is_element_present(self, how, what):
+    #    try: self.wd.find_element(by=how, value=what)
+    #    except NoSuchElementException as e: return False
+    #    return True
     
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
+    #def is_alert_present(self):
+    #    try: self.wd.switch_to_alert()
+    #    except NoAlertPresentException as e: return False
+    #    return True
     
     def tearDown(self):
         self.wd.quit()
