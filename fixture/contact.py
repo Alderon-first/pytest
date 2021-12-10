a436624eb5c5601c7862f6166d67cdb1b482b1fb
Alderon <reidzilag@mail.ru>
2021-12-10 15:00:18 +0300

    добавлен тест на изменение контакта

--- fixture/contact.py
+++ fixture/contact.py
@@ -27,6 +27,32 @@ class ContactHelper:
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
         self.return_contact_page()
 
+    def change_first_contact(self, contact):
+        wd = self.app.wd
+        # выбрать первую
+        wd.find_element_by_name("selected[]").click()
+        # изменить выбранную
+        wd.find_element_by_xpath("/html/body/div/div[4]/form[2]/table/tbody/tr[2]/td[8]/a/img").click()
+        # fill contact form
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.middlename)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact.mobile)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.email)
+        # submit contact creation
+        wd.find_element_by_xpath("/html/body/div/div[4]/form[1]/input[22]").click()
+        self.return_contact_page()
+
     def delete_first_contact(self):
         wd = self.app.wd
         #выбрать первую
---
+++ test/test_change_contact.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from model.contact import Contact
+
+
+def test_change_first_contact(app):
+    app.session.login(password="secret", username="admin")
+    app.contact.change_first_contact(Contact(firstname="Name 4", middlename="Name 5", lastname="Name 6", mobile="9-888-88-88-88",
+                               email="222@222.ru"))
+    app.session.logout()
+
