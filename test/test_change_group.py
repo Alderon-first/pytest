015e1b87343aea014a3857393e07ef42b2326381
Alderon <reidzilag@mail.ru>
2021-12-13 08:56:41 +0300

    добавлена проверка на существование группы/контакта

--- test/test_change_group.py
+++ test/test_change_group.py
@@ -3,6 +3,6 @@ from model.group import Group
 
 
 def test_change_first_group(app):
-    if app.contact.count() == 0:
-        app.contact.create(Group(name="group32", header="header32", footer="footer32"))
+    if app.group.count() == 0:
+        app.group.create(Group(name="group32", header="header32", footer="footer32"))
     app.group.change_first_group(Group(name="group1", header="header1", footer="footer1"))
--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -3,6 +3,6 @@ from model.contact import Contact
 
 def test_delete_first_contact(app):
     if app.contact.count() == 0:
-        app.group.create(Contact(firstname="Name 1", middlename="Name 2", lastname="Name 3", mobile="8-888-88-88-88",
+        app.contact.create(Contact(firstname="Name 1", middlename="Name 2", lastname="Name 3", mobile="8-888-88-88-88",
                                  email="111@111.ru"))
     app.contact.delete_first_contact()
