30f5e192ac1cd1c71b540354cc3580b424b8e303
Alderon <reidzilag@mail.ru>
2021-12-21 14:24:47 +0300

    убрана задержка при открыти браузера добавлена проверка на метод "вернуться на страницу групп"

--- fixture/group.py
+++ fixture/group.py
@@ -6,7 +6,7 @@ class GroupHelper:
     def open_groups_page(self):
         # open groups page
         wd = self.app.wd
-        if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new"))>0):
+        if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new")) > 0):
             wd.find_element_by_link_text("groups").click()
 
     def create(self, group):
@@ -62,9 +62,11 @@ class GroupHelper:
     def return_to_groups_page(self):
         # return groups page
         wd = self.app.wd
-        wd.find_element_by_link_text("group page").click()
+        if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new")) > 0):
+            wd.find_element_by_link_text("group page").click()
 
     def count(self):
         wd = self.app.wd
         self.open_groups_page()
         return len(wd.find_elements_by_name("selected[]"))
+
