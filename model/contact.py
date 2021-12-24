80179f0004d7fefa3611d1c8f410b62aa45d2b6f
Alderon <reidzilag@mail.ru>
2021-12-24 14:10:30 +0300

    добавлена проверка сортированного списка контактов при добавлении контакта

--- model/contact.py
+++ model/contact.py
@@ -1,3 +1,6 @@
+from sys import maxsize
+
+
 class Contact:
     def __init__(self, firstname, middlename, lastname, mobile, email, id_contact):
         self.firstname = firstname
@@ -12,8 +15,17 @@ class Contact:
         # это про вывод информации на консоль
 
     def __eq__(self, other):
-        return self.id_contact == other.id_contact \
+        return (self.id_contact is None
+                or other.id_contact is None
+                or self.id_contact == other.id_contact) \
                and self.firstname == other.firstname \
                and self.lastname == other.lastname
         # это про логическое сравнение параметров (чтобы сравнивалось значение, а не значение+расположение информации)
+# если у контакта есть id, возвращаем его, если нет - возвращаем максимальное число.
+    # этот метод нужен для правильной сортировки
 
+    def id_or_max(self):
+        if self.id_contact:
+            return int(self.id_contact)
+        else:
+            return maxsize
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -4,8 +4,10 @@ from model.contact import Contact
 
 def test_add_contact(app):
     old_groups = app.contact.get_contacts_list()
-    app.contact.create(
-        Contact(firstname="Name 1", middlename="Name 2", lastname="Name 3", mobile="8-888-88-88-88", email="111@111.ru",
-                id_contact=None))
+    contact = Contact(firstname="Name 1", middlename="Name 2", lastname="Name 3",
+                      mobile="8-888-88-88-88", email="111@111.ru", id_contact=None)
+    app.contact.create(contact)
     new_groups = app.contact.get_contacts_list()
     assert len(old_groups)+1 == len(new_groups)
+    old_groups.append(contact)
+    assert sorted(old_groups, key=Contact.id_or_max) == sorted(new_groups, key=Contact.id_or_max)
