e4c556b90efda4487db6a54e28d4bc3054df9040
Alderon <reidzilag@mail.ru>
2021-12-24 16:25:43 +0300

    добавлен рандомный выбор групы для  зменения группы

--- fixture/group.py
+++ fixture/group.py
@@ -55,6 +55,29 @@ class GroupHelper:
         self.return_to_groups_page()
         self.group_cache = None
 
+    def change_group_by_index(self, index, group):
+        # create group
+        wd = self.app.wd
+        self.open_groups_page()
+        # выбрать первую
+        self.select_group_by_index(index)
+        # нажать изменить
+        wd.find_element_by_name("edit").click()
+        # fill groups +
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        # submit group creation
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+        self.group_cache = None
+
     def delete_first_group(self):
         wd = self.app.wd
         self.open_groups_page()
--- test/test_change_group.py
+++ test/test_change_group.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
+from random import randrange
 
 
 def test_change_first_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="group32", header="header32", footer="footer32"))
     old_groups = app.group.get_group_list()
+    index = randrange(len(old_groups))
     group = Group(name="group1", header="header1", footer="footer1")
-    group.id_group = old_groups[0].id_group
+    group.id_group = old_groups[index].id_group
     # тут сохраняем id группы, которую будем менять
-    app.group.change_first_group(group)
+    app.group.change_group_by_index(index, group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
-    old_groups[0] = group
+    old_groups[index] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
