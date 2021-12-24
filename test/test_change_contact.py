f755f72f772886fc11fdd49394eeb13bde3f032a
Alderon <reidzilag@mail.ru>
2021-12-24 14:13:59 +0300

    добавлена проверка сортированного списка контактов при изменении контакта

--- test/test_change_contact.py
+++ test/test_change_contact.py
@@ -7,8 +7,11 @@ def test_change_first_contact(app):
         app.contact.create(Contact(firstname="Name 1", middlename="Name 2", lastname="Name 3", mobile="8-888-88-88-88",
                                    email="111@111.ru", id_contact=None), )
     old_groups = app.contact.get_contacts_list()
-    app.contact.change_first_contact(
-        Contact(firstname="Name 4", middlename="Name 5", lastname="Name 6", mobile="9-888-88-88-88", email="222@222.ru",
-                id_contact=None))
+    contact = Contact(firstname="Name 4", middlename="Name 5", lastname="Name 6",
+                      mobile="9-888-88-88-88", email="222@222.ru", id_contact=None)
+    contact.id_contact = old_groups[0].id_contact
+    app.contact.change_first_contact(contact)
     new_groups = app.contact.get_contacts_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = contact
+    assert sorted(old_groups, key=Contact.id_or_max) == sorted(new_groups, key=Contact.id_or_max)
