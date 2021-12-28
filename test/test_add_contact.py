b6dcfbabaa8b538f0c4fe2aa1e936b90d34c37bc
Alderon <reidzilag@mail.ru>
2021-12-28 16:52:18 +0300

    исправлено название переменной для группы контактов в тесте на добавление контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,11 +3,11 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    old_groups = app.contact.get_contacts_list()
+    old_contacts = app.contact.get_contacts_list()
     contact = Contact(firstname="Name 1", middlename="Name 2", lastname="Name 3",
                       mobile="8-888-88-88-88", email="111@111.ru", id_contact=None)
     app.contact.create(contact)
-    new_groups = app.contact.get_contacts_list()
-    assert len(old_groups)+1 == app.contact.count()
-    old_groups.append(contact)
-    assert sorted(old_groups, key=Contact.id_or_max) == sorted(new_groups, key=Contact.id_or_max)
+    new_contacts = app.contact.get_contacts_list()
+    assert len(old_contacts)+1 == app.contact.count()
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
