ec24ac04b5c65a2ef00e7fa235f641eb651e9b64
Alderon <reidzilag@mail.ru>
2022-01-10 02:35:24 +0300

    добавлена возможность указать через параметры путь к файлу, куда сохранятся данные и количество созданных данных для групп и контактов

--- generator/contact.py
+++ generator/contact.py
@@ -3,6 +3,24 @@ import random
 import string
 import os.path
 import json
+import getopt
+import sys
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of contacts", "file"])
+except getopt.GetoptError:
+    getopt.usage()
+    sys.exit(2)
+
+n = 2
+# quantity_tests_for_parameter = n
+f = "data/contacts.json"
+
+for o, a in opts:
+    if o == "-n":
+        n = int(a)
+    elif o == "-f":
+        f = a
 
 
 def random_string(prefix, maxlen):
@@ -22,11 +40,11 @@ testdata = [Contact(firstname="", middlename="", lastname="",
             lastname=random_string("lastname", 10), mobile=random_numb("", 10), work_telephone=random_numb("", 10),
             home_telephone=random_numb("", 10), email=random_string("email", 10), email2=random_string("email2", 10),
             address=random_string("address", 10), id_contact=None)
-    for i in range(5)
+    for i in range(n)
 ]
 
 
-file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../data/contacts.json")
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
 with open(file, "w") as f:
     f.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
