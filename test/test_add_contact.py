0ad16c0ddaa6e31ec33c36a1ccce72269d36694b
Alderon <reidzilag@mail.ru>
2022-01-09 21:58:36 +0300

    теперь тест на проверку мобильного игнорирует некоторые символы,такие как ()

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -5,7 +5,7 @@ from model.contact import Contact
 def test_add_contact(app):
     old_contacts = app.contact.get_contacts_list()
     contact = Contact(firstname="Name 1", middlename="Name 2", lastname="Name 3",
-                      mobile="8888888888", email="111@111.ru", id_contact=None)
+                      mobile="8-888-88-88-88", email="111@111.ru", id_contact=None)
     app.contact.create(contact)
     new_contacts = app.contact.get_contacts_list()
     assert len(old_contacts)+1 == app.contact.count()
--- test/test_pfones.py
+++ test/test_pfones.py
@@ -1,4 +1,11 @@
+import re
+
 def test_phones_on_home_page(app):
     contact_from_home_page = app.contact.get_contacts_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_home_page.mobile == contact_from_edit_page.mobile
+    assert contact_from_home_page.mobile == clear(contact_from_edit_page.mobile)
+
+
+def clear(s):
+    return re.sub("[() -]", "", s)
+
