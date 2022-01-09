0545dceb571ee041786e9a18ea94c562c338ca67
Alderon <reidzilag@mail.ru>
2022-01-09 21:50:06 +0300

    создан тест проверки мобильного телефона

--- fixture/contact.py
+++ fixture/contact.py
@@ -49,7 +49,7 @@ class ContactHelper:
         self.open_home_page()
         self.open_contact_by_index(index)
         # изменить выбранную
-        #wd.find_element_by_xpath("/html/body/div/div[4]/form[2]/table/tbody/tr[2]/td[8]/a/img").click()
+        # wd.find_element_by_xpath("/html/body/div/div[4]/form[2]/table/tbody/tr[2]/td[8]/a/img").click()
         # fill contact form
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
@@ -78,7 +78,14 @@ class ContactHelper:
         row = wd.find_elements_by_name("entry")[index]
         cell = row.find_elements_by_tag_name("td")[7]
         cell.find_element_by_tag_name("a").click()
-        #wd.find_elements_by_name("selected[]")[index].click()
+        # wd.find_elements_by_name("selected[]")[index].click()
+
+    def open_contact_view_by_index(self, index):
+        wd = self.app.wd
+        self.app.open_home_page()
+        row = wd.find_elements_by_name("entry")[index]
+        cell = row.find_elements_by_tag_name("td")[6]
+        cell.find_element_by_tag_name("a").click()
 
     def select_contact_by_index(self, index):
         wd = self.app.wd
@@ -126,6 +133,18 @@ class ContactHelper:
                 lastname = element.find_elements_by_css_selector("td")[1].text
                 firstname = element.find_elements_by_css_selector("td")[2].text
                 id_contact = element.find_element_by_name("selected[]").get_attribute("value")
+                all_phones = element.find_elements_by_css_selector("td")[5].text.splitlines()
                 self.contacts_cache.append(Contact(firstname=firstname, middlename=None, lastname=lastname,
-                                                   mobile=None, email=None, id_contact=id_contact))
+                                                   mobile=all_phones[0], email=None, id_contact=id_contact))
         return list(self.contacts_cache)
+
+    def get_contact_info_from_edit_page(self, index):
+        wd = self.app.wd
+        self.open_contact_by_index(index)
+        firstname = wd.find_element_by_name("firstname").get_attribute('value')
+        lastname = wd.find_element_by_name("lastname").get_attribute('value')
+        id_contact = wd.find_element_by_name("id").get_attribute('value')
+        mobile = wd.find_element_by_name("mobile").get_attribute('value')
+        return Contact(firstname=firstname, lastname=lastname, id_contact=id_contact, mobile=mobile,
+                       email=None, middlename=None)
+
