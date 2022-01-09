e4a10e8cdbfa06fcda6ccdb57c817a42e0ca6ec6
Alderon <reidzilag@mail.ru>
2022-01-10 02:36:57 +0300

    исправлено задвоение переменных в генераторе

--- generator/contact.py
+++ generator/contact.py
@@ -46,5 +46,5 @@ testdata = [Contact(firstname="", middlename="", lastname="",
 
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
-with open(file, "w") as f:
-    f.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
+with open(file, "w") as out:
+    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
